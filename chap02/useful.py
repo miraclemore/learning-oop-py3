bbeeb61c9801e5ccde676a3867e704c672e19c90
miracle.huang <miraclemore.huang@gmail.com>
2022-08-14 22:16:29 +0800

    update useful class

---
+++ chap02/useful.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Useful:
+    """This is class might be useful to other modules"""
+    pass
+
+
+def main():
+    """create a useful class and does sth with it for our modules"""
+    useful = Useful()
+    print(useful)
+
+
+if __name__ == '__main__':
+    main()
